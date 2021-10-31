1076af429f42e4e46379266850121a30c077e4e6
XcQuid <creed.35@yandex.ru>
2021-10-31 21:55:56 +0300

    feat: добавить скрипты редактирования профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button')
-const closeButton = document.querySelector('.popup__close-button')
+const closeButton = popup.querySelector('.popup__close-button')
+const formElement = popup.querySelector('.popup__container')
+const nameInput = formElement.querySelector('.popup__name')
+const descriptionInput = formElement.querySelector('.popup__description')
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description')
+
+nameInput.value = profileName.textContent;
+descriptionInput.value = profileDescription.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  popupClose();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
 
 function popupOpen() {
   popup.classList.add('popup__opened')
-}
+};
 
 function popupClose() {
   popup.classList.remove('popup__opened')
-}
-
-
+};
 
 editButton.addEventListener('click', popupOpen)
 closeButton.addEventListener('click', popupClose)
