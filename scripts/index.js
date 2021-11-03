6eb617407d3507f6a852dbb76036652f4f870960
XcQuid <creed.35@yandex.ru>
2021-11-03 20:51:36 +0300

    feat: добавить активное состояние кнопки лайк

--- scripts/index.js
+++ scripts/index.js
@@ -6,34 +6,33 @@ const nameInput = formElement.querySelector('.popup__input_type_name');
 const descriptionInput = formElement.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-
-nameInput.value = profileName.textContent;
-descriptionInput.value = profileDescription.textContent;
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-  popupClose();
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-formElement.addEventListener('keyup', (evt) => {
-  if (evt.key === 13) {
-    formSubmitHandler();
-  }
-})
+const likeButton = document.querySelectorAll('.button_type_like');
 
 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 };
 
 function popupClose() {
   popup.classList.remove('popup_opened');
 };
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  popupClose();
+};
+
+for (let i = 0; i < likeButton.length; i = i + 1) {
+  likeButton[i].addEventListener('click', function() {
+    likeButton[i].classList.toggle('button_type_like-active')
+  })
+}
+
+
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
-
+formElement.addEventListener('submit', formSubmitHandler);
