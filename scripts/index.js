522ded87d59287bf028103b358105f190e0a8b23
XcQuid <creed.35@yandex.ru>
2021-11-04 14:18:14 +0300

    fix: Исправления после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,16 @@
 const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.button_type_edit-profile');
-const closeButton = popup.querySelector('.button_type_popup-close');
-const formElement = popup.querySelector('.popup__container');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__input_type_name');
 const descriptionInput = formElement.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const likeButton = document.querySelectorAll('.button_type_like');
-
 
 function popupOpen() {
-  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
 };
 
 function popupClose() {
@@ -25,12 +23,6 @@ function formSubmitHandler(evt) {
   profileDescription.textContent = descriptionInput.value;
   popupClose();
 };
-
-for (let i = 0; i < likeButton.length; i = i + 1) {
-  likeButton[i].addEventListener('click', function() {
-    likeButton[i].classList.toggle('button_type_like-active')
-  })
-}
 
 
 editButton.addEventListener('click', popupOpen);
