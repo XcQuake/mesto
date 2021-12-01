fc803c2dc0256dc0695d6510bbcaabecb7379661
XcQuid <creed.35@yandex.ru>
2021-12-01 17:59:06 +0300

    refactor: удалит лишний слушатель

--- scripts/validate.js
+++ scripts/validate.js
@@ -54,10 +54,6 @@ const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.popup__form'));
 
   formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function(evt) {
-      evt.preventDefault();
-    });
-
     setEventListeners(formElement);
   });
 };
