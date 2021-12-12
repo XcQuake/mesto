4b2f067da131219f8dab541381e7e10a2eb7d2eb
XcQuid <creed.35@yandex.ru>
2021-12-12 15:41:25 +0300

    refactor: перенести массив карточек и конфиг валидатора в отдельный файл

---
+++ scripts/constants.js
@@ -0,0 +1,36 @@
+const initialCards = [
+  {
+    title: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    title: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    title: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    title: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    title: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    title: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const validateConfig = {
+  form: '.popup__form',
+  inputSelector: '.popup__input',
+  buttonSelector: '.popup__save-button',
+  buttonInactiveClass: 'popup__save-button_inactive',
+  activeErrorClass: 'popup__input-error_active',
+  inputErrorClass: 'popup__input_error'
+}
+
+export {initialCards, validateConfig};
