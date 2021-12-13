066a9d5c332842f0522f058843b78c2e0df3ca0b
XcQuid <creed.35@yandex.ru>
2021-12-13 12:12:21 +0300

    refactor: перенести элементы карточки в поля класса

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,27 +1,34 @@
-import openPopup from './index.js'
+import openPopup from './index.js';
+import {
+  popupTypeImage,
+  popupImage,
+  popupCaption
+} from './constants.js';
 
 export default class Card {
   constructor(data, cardSelector) {
     this._title = data.title;
     this._link = data.link;
-
     this._cardSelector = cardSelector;
+    
+    this._element = this._getTemplate();
+    this._likeButton = this._element.querySelector('.card__like-button');
+    this._deleteButton = this._element.querySelector('.card__delete-button');
+    this._cardImage = this._element.querySelector('.card__image');
+    this._cardTitle = this._element.querySelector('.card__title');
   }
 
   _getTemplate() {
     const cardElement = document
       .querySelector(this._cardSelector)
       .content
+      .querySelector('.card')
       .cloneNode(true);
 
     return cardElement
   }
 
   _openImagePopup() {
-    const popupTypeImage = document.querySelector('.popup_type_image');
-    const popupImage = document.querySelector('.popup__image');
-    const popupCaption = document.querySelector('.popup__image-caption');
-
     popupImage.src = this._link;
     popupImage.alt = this._title;
     popupCaption.textContent = this._title;
@@ -30,30 +37,25 @@ export default class Card {
   }
 
   _setEventListeners() {   
-    const likeButton = this._element.querySelector('.card__like-button');
-    const deleteButton = this._element.querySelector('.card__delete-button');
-    const cardImage = this._element.querySelector('.card__image');
-
-    likeButton.addEventListener('click', () => {
-      likeButton.classList.toggle('card__like-button_active');
+    this._likeButton.addEventListener('click', () => {
+      this._likeButton.classList.toggle('card__like-button_active');
     })
 
-    deleteButton.addEventListener('click', () => {
-      deleteButton.closest('.card').remove();
+    this._deleteButton.addEventListener('click', () => {
+      this._element.remove();
     })
 
-    cardImage.addEventListener('click', () => {
+    this._cardImage.addEventListener('click', () => {
       this._openImagePopup();
     })
   }
 
   generateCard() {
-    this._element = this._getTemplate();
     this._setEventListeners();
 
-    this._element.querySelector('.card__image').src = this._link;
-    this._element.querySelector('.card__image').alt = this._title;
-    this._element.querySelector('.card__title').textContent = this._title;
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._title;
+    this._cardTitle.textContent = this._title;
 
     return this._element;
   }
