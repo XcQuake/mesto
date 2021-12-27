c9fcc7f592e53d939b4d8767e6e472ef0df75ec2
XcQuid <creed.35@yandex.ru>
2021-12-27 11:53:20 +0300

    feat: создать методы закрытия попапа

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -1,24 +1,37 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
-    this._element = this._getPopup();
+    this._element = document.querySelector(this._popupSelector);
+
+    this._handleMousedownClose = this._handleMousedownClose.bind(this);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
-  _getPopup() {
-    this._popup = document.querySelector(this._popupSelector)
-    return this._popup;
-  }
+  _handleMousedownClose(evt) {
+    if (evt.target.classList.contains('popup__close-button') 
+    || evt.target.classList.contains('popup_opened')) {
+      this.close();
+    }
+  };
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  };
+
+  setEventListener() {
+    this._element.addEventListener('mousedown', this._handleMousedownClose);
+    document.addEventListener('keydown', this._handleEscClose);
+  };
 
   open() {
     this._element.classList.add('popup_opened');
   };
 
   close() {
-    this._popup.classList.remove('popup_opened');
-  };
-
-  setEventListener() {
-    this._closeButton = this._popup.querySelector('.popup__close-button');
-    this._closeButton.setEventListener('click', this.close());
+    this._element.classList.remove('popup_opened');
+    this._element.removeEventListener('mousedown', this._handleMousedownClose);
+    document.removeEventListener('keydown', this._handleEscClose);
   };
 }
