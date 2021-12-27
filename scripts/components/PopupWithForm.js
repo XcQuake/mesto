e29b1c1143ddd9157b579e832cc335fe47383d7e
XcQuid <creed.35@yandex.ru>
2021-12-27 11:51:15 +0300

    feat: создать метод _getInputValues

--- scripts/components/PopupWithForm.js
+++ scripts/components/PopupWithForm.js
@@ -1,23 +1,42 @@
 import Popup from './Popup.js'
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, submitForm) {
+  constructor({submitForm, resetForm}, popupSelector) {
     super(popupSelector);
-    this._element = super._getPopup();
-    this._form = this._element.querySelector('.popup__form')
-    this._submitButton = this._element.querySelector('.popup__save-button');
-
+    super._element;
+    this._form = this._element.querySelector('.popup__form');
+    
     this._submitForm = submitForm;
+    this._resetForm = resetForm;
+    this._inputvalues = this._getInputValues();
+    this._handler = this._handler.bind(this);
+  }
+
+  _getInputValues() {
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    this._data = {};
+
+    this._inputList.forEach((input) => {
+      this._data[input.name] = input.value;
+    })
+
+    return this._data;
+  }
+
+  _handler(event) {
+    event.preventDefault();
+    this._submitForm(this._getInputValues());
+    this.close();
   }
 
   setEventListener() {
-    this._submitButton.addEventListener('click', this._submitForm);
-
+    this._form.addEventListener('submit', this._handler);
     super.setEventListener();
   }
 
   close() {
-    this._form.reset();
+    this._form.removeEventListener('submit', this._handler);
+    this._resetForm();
     super.close();
   }
 } 
