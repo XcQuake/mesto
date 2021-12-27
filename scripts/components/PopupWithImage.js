3650e3ad5b6abffc74de5cf3f0fa6b32bbf5e896
XcQuid <creed.35@yandex.ru>
2021-12-27 11:47:56 +0300

    fix: исправить отображение навзания картинки в попапе

--- scripts/components/PopupWithImage.js
+++ scripts/components/PopupWithImage.js
@@ -3,9 +3,9 @@ import Popup from './Popup.js'
 export default class PopupWithImage extends Popup {
   constructor(popupSelector, image) {
     super(popupSelector);
+    super._element;
     this._link = image.src;
     this._title = image.alt;
-    this._element = super._getPopup()
 
     this._image = this._element.querySelector('.popup__image');
     this._caption = this._element.querySelector('.popup__image-caption');
@@ -14,7 +14,7 @@ export default class PopupWithImage extends Popup {
   open() {
     this._image.src = this._link;
     this._image.alt = this._title;
-    this._caption.textcontent = this._title;
+    this._caption.textContent = this._title;
 
     super.open();
   }
