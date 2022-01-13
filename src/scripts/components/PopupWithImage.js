6c99c2974dc6eb47078df11090c45f11cb6188bc
XcQuid <creed.35@yandex.ru>
2022-01-13 16:42:14 +0300

    style: удалить лишний аргумент в PopupWithImage

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,7 +1,7 @@
 import Popup from './Popup.js'
 
 export default class PopupWithImage extends Popup {
-  constructor(popupSelector, image) {
+  constructor(popupSelector) {
     super(popupSelector);
     super._element;
 
